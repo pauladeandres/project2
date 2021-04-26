b95d3a70db0df4961325f1836eee8b9b77d088b5
Achraf <Achrafarazzouk@gmail.com>
2021-04-27 00:26:14 +0200

    Few improvements

--- models/restaurant.model.js
+++ models/restaurant.model.js
@@ -19,24 +19,31 @@ const restaurantSchema = new Schema({
         maxlength: 500
     },
 
-    specialities: {[
-        pizza: { type: Boolean, required: true, default: false },
-        hamburguer: { type: Boolean, required: true, default: false },
-        sushi: { type: Boolean, required: true, default: false },
-        chinese: { type: Boolean, required: true, default: false },
-        veggie: { type: Boolean, required: true, default: false },
-        japanese: { type: Boolean, required: true, default: false },
-        poke: { type: Boolean, required: true, default: false },
-        dessert: { type: Boolean, required: true, default: false },
-        spanish: { type: Boolean, required: true, default: false },
-        italian: { type: Boolean, required: true, default: false },
-        tapas: { type: Boolean, required: true, default: false },
-        pasta: { type: Boolean, required: true, default: false },
-        kebab: { type: Boolean, required: true, default: false },
-        mexican: { type: Boolean, required: true, default: false },
-        salad: { type: Boolean, required: true, default: false },
-        indian: { type: Boolean, required: true, default: false }
-    ]},
+    // specialities: {[
+    //     pizza: { type: Boolean, required: true, default: false },
+    //     hamburguer: { type: Boolean, required: true, default: false },
+    //     sushi: { type: Boolean, required: true, default: false },
+    //     chinese: { type: Boolean, required: true, default: false },
+    //     veggie: { type: Boolean, required: true, default: false },
+    //     japanese: { type: Boolean, required: true, default: false },
+    //     poke: { type: Boolean, required: true, default: false },
+    //     dessert: { type: Boolean, required: true, default: false },
+    //     spanish: { type: Boolean, required: true, default: false },
+    //     italian: { type: Boolean, required: true, default: false },
+    //     tapas: { type: Boolean, required: true, default: false },
+    //     pasta: { type: Boolean, required: true, default: false },
+    //     kebab: { type: Boolean, required: true, default: false },
+    //     mexican: { type: Boolean, required: true, default: false },
+    //     salad: { type: Boolean, required: true, default: false },
+    //     indian: { type: Boolean, required: true, default: false }
+    // ]},
+
+
+    specialties: {
+        type: String,
+        enum: ['pizza', 'hamburguer', 'sushi', 'chinese', 'veggie', 'japanese', 'poke', 'dessert', 'spanish', 'italian', 'tapas', 'pasta', 'kebab', 'mexican', 'salad', 'indian'],
+        required: true
+    },
 
     menu: [{
         type: Schema.Types.ObjectId,
--- routes/restaurant.routes.js
+++ routes/restaurant.routes.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const Restaurant = require('./../models/restaurant.model')
+const Dish = require('./../models/dish.model')
 
-router.get('/', (req, res) => res.render('pages/restaurants/index'))
+router.get('/', (req, res) => res.render('pages/restaurants/index')) //TODO
 
 //router.get('/map') TODO
 ""
-router.get('/detail', (req, res) => {
+router.get('/detail/:id', (req, res) => {
 
     const { id } = req.params
 
@@ -17,28 +18,31 @@ router.get('/detail', (req, res) => {
 //router.use((req, res, next) => req.session.currentUser ? next() : res.redirect('/login'))
 
 
-router.get('/create', isLoggedIn, checkRoles('OWNER'), (req, res) => res.render('pages/restaurants/create-form'))
+router.get('/create', /**isLoggedIn, checkRoles('OWNER'),**/(req, res) => res.render('pages/restaurants/create-form'))
 
-router.post('/create', isLoggedIn, checkRoles('OWNER'), (req, res) => {
+router.post('/create', /**isLoggedIn, checkRoles('OWNER'),**/(req, res) => {
     let { name, profileImg, description, specialties } = req.body
 
     Restaurant.create({ name, profileImg, description, specialties }).then(() => res.redirect('/restaurants')).catch(err => console.log('error', err))
 })
 
 
-router.get('edit/:id', isLoggedIn, checkRoles('OWNER'), (req, res) => {
+router.get('/edit/:id', /**isLoggedIn, checkRoles('OWNER'),**/(req, res) => {
 
-    const { rest_id } = req.params
+    const { rest_id } = req.params.id
 
     Restaurant.findById(rest_id).then(elem => res.render('pages/restaurants/edit-form', elem)).catch(err => console.log('Error!', err))
 })
 
-router.post('edit/:id', isLoggedIn, checkRoles('OWNER'), (req, res) => {
+router.post('/edit/:id', /**isLoggedIn, checkRoles('OWNER'),**/(req, res) => {
 
-    const { rest_id } = req.params
+    const { id } = req.params
     const { name, profileImg, description, specialties } = req.body
-
-    Restaurant.findByIdAndUpdate(rest_id, { name, profileImg, description, specialties }).then(elem => {
-        res.redirect(`/restaurants/details/${elem._id}`)
+    console.log(id);
+    Restaurant.findByIdAndUpdate(id, { name, profileImg, description, specialties }).then(elem => {
+        console.log(elem);
+        res.redirect(`/restaurants/detail/${elem._id}`)
     }).catch(err => console.log('Error!', err))
 })
+
+module.exports = router
