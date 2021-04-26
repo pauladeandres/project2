fcabad079332e95e216552545e6de6cad209ea9f
Achraf <Achrafarazzouk@gmail.com>
2021-04-26 16:54:47 +0200

    routes done

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ module.exports = app => {
 
     // Base URLS
     app.use('/', require('./base.routes.js'))
+    app.use('/restaurants', require('./restaurant.routes.js'))
     app.use('/auth', require('./auth.routes.js'))
 }
